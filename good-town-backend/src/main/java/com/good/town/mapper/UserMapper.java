8780ea7fbf27623626f7368095107073e8c2a3ce
zhuimengliuli <1428278690@qq.com>
2024-11-15 20:07:49 +0800

    实现登录和注册功能

---
+++ good-town-backend/src/main/java/com/good/town/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.good.town.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.good.town.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
