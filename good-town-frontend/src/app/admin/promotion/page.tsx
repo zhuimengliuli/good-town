e88cf62a495ef2e520d2a323ae5f1fb117337b21
zhuimengliuli <1428278690@qq.com>
2024-12-07 15:27:17 +0800

    补充管理员宣传模块

--- good-town-frontend/src/app/admin/promotion/page.tsx
+++ good-town-frontend/src/app/admin/promotion/page.tsx
@@ -1,17 +1,9 @@
 "use client";
-import { PlusOutlined } from "@ant-design/icons";
-import {
-  ActionType,
-  PageContainer,
-  ProColumns,
-  ProTable,
-} from "@ant-design/pro-components";
-import { Button, message, Space, Typography } from "antd";
-import { useRef, useState } from "react";
-import {
-  deleteAssistanceUsingPost,
-  listAssistanceByPageUsingPost,
-} from "@/api/assistanceController";
+import {PlusOutlined} from "@ant-design/icons";
+import {ActionType, PageContainer, ProColumns, ProTable,} from "@ant-design/pro-components";
+import {Button, message, Space, Typography} from "antd";
+import {useRef, useState} from "react";
+import {deletePromotionUsingPost, listPromotionByPageUsingPost,} from "@/api/promotionController";
 import CreateModal from "@/app/admin/assistance/components/CreateModal";
 import UpdateModal from "@/app/admin/assistance/components/UpdateModal";
 
@@ -20,25 +12,25 @@ import UpdateModal from "@/app/admin/assistance/components/UpdateModal";
  *
  * @constructor
  */
-const AssistanceAdminPage: React.FC = () => {
+const PromotionAdminPage: React.FC = () => {
   // 是否显示新建窗口
   const [createModalVisible, setCreateModalVisible] = useState<boolean>(false);
   // 是否显示更新窗口
   const [updateModalVisible, setUpdateModalVisible] = useState<boolean>(false);
   const actionRef = useRef<ActionType>();
   // 当前用户点击的数据
-  const [currentRow, setCurrentRow] = useState<API.Assistance>();
+  const [currentRow, setCurrentRow] = useState<API.Promotion>();
 
   /**
    * 删除节点
    *
    * @param row
    */
-  const handleDelete = async (row: API.Assistance) => {
+  const handleDelete = async (row: API.Promotion) => {
     const hide = message.loading("正在删除");
     if (!row) return true;
     try {
-      await deleteAssistanceUsingPost({
+      await deletePromotionUsingPost({
         id: row.id as any,
       });
       hide();
@@ -52,7 +44,7 @@ const AssistanceAdminPage: React.FC = () => {
     }
   };
 
-  const columns: ProColumns<API.Assistance>[] = [
+  const columns: ProColumns<API.Promotion>[] = [
     {
       title: "id",
       dataIndex: "id",
@@ -60,7 +52,12 @@ const AssistanceAdminPage: React.FC = () => {
       hideInForm: true,
     },
     {
-      title: "助力描述",
+      title: "主题名称",
+      dataIndex: "themeName",
+      valueType: "textarea",
+    },
+    {
+      title: "描述",
       dataIndex: "description",
       valueType: "textarea",
     },
@@ -95,7 +92,7 @@ const AssistanceAdminPage: React.FC = () => {
   ];
   return (
     <PageContainer>
-      <ProTable<API.Assistance>
+      <ProTable<API.Promotion>
         headerTitle={"查询表格"}
         actionRef={actionRef}
         rowKey="key"
@@ -117,12 +114,12 @@ const AssistanceAdminPage: React.FC = () => {
           const sortField = Object.keys(sort)?.[0];
           const sortOrder = sort?.[sortField] ?? undefined;
 
-          const  res = await listAssistanceByPageUsingPost({
+          const  res = await listPromotionByPageUsingPost({
             ...params,
             sortField,
             sortOrder,
             ...filter,
-          } as API.AssistanceQueryRequest);
+          } as API.PromotionQueryRequest);
           const data = res.data;
           const code = res.code;
           return {
@@ -160,4 +157,4 @@ const AssistanceAdminPage: React.FC = () => {
     </PageContainer>
   );
 };
-export default AssistanceAdminPage;
+export default PromotionAdminPage;
